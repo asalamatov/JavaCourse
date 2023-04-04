79b6ae6ef3838c9ed7c0540c5c561ae6e6049635
Azamat Salamatov <azamat.salamatov2004@gmail.com>
2023-04-04 09:42:43 -0500

    finished Java unittesting

--- GuessingGame/src/main/org/aztech/game/GuessingGame.java
+++ GuessingGame/src/main/org/aztech/game/GuessingGame.java
@@ -4,12 +4,47 @@ import java.security.SecureRandom;
 
 public class GuessingGame {
     private final int randomNumber = new SecureRandom().nextInt(10)+1;
+    private int counter = 0;
 
     public String guess(int guessedNumber) {
-        return (guessedNumber==getRandomNumber() ? "You got it" : "Game over");
+        counter ++;
+        String tryTries = (counter==1) ? "try" : "tries";
+        String winningMessage = String.format("You got it in %d %s", counter, tryTries);
+        String response = null;
+        if (counter == 4 && guessedNumber!=getRandomNumber()){
+            response =  String.format("Game over and you've had %d %s.",counter, tryTries);
+        } else if (counter>4){
+            response = "Sorry, you are limited to only 4 tries.";
+        }else{
+            String tooHighLowText = null;
+            if (guessedNumber < getRandomNumber()){
+                tooHighLowText = " - you are too low";
+            } else{
+                tooHighLowText = " - you are too high";
+            }
+            String loseText = String.format("Game over%s", tooHighLowText);
+            response = (guessedNumber==getRandomNumber() ? winningMessage : loseText);
+        }
+        return response;
     }
 
     public int getRandomNumber() {
         return randomNumber;
+    }
+
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        boolean loopShouldContinue = true;
+        do {
+            String input = System.console().readLine("Enter a number: ");
+            if ("q".equals(input)){
+                return;
+            }
+            String output = game.guess(Integer.parseInt(input));
+            if (output.contains("You got it" ) || output.contains("over")){
+                loopShouldContinue = false;
+            }
+            
+        } while (loopShouldContinue);
     }
 }
--- GuessingGame/src/test/org/aztech/game/GuessingGameTest.java
+++ GuessingGame/src/test/org/aztech/game/GuessingGameTest.java
@@ -1,13 +1,16 @@
 package org.aztech.game;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GuessingGameTest {
 
+    public static final int GAME_RANDOMNESS_RETRIES = 100;
     private GuessingGame game;
 
     @BeforeEach
@@ -19,26 +22,27 @@ public class GuessingGameTest {
     public void testSimpleWinSituation(){
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum);
-        assertEquals("You got it", message);
+        assertEquals("You got it in 1 try", message);
     }
 
     @Test
     public void testOneWrongNegGuessSituation(){
         String message = game.guess(-5);
-        assertEquals("Game over", message);
+        assertEquals("Game over - you are too low", message);
     }
 
     @Test
     public void testOneWrongPosGuessSituation(){
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum+1);
-        assertEquals("Game over", message);
+        assertEquals("Game over - you are too high", message);
     }
 
-    @RepeatedTest(10)
+//    @RepeatedTest(10)
+    @Test
     public void testRandomNumberGeneration(){
         int[] rndNumCount = new int[11];
-        for (int counter=0; counter<60; counter++){
+        for (int counter = 0; counter< GAME_RANDOMNESS_RETRIES; counter++){
             GuessingGame gameLocal = new GuessingGame();
             int randomNum = gameLocal.getRandomNumber();
             rndNumCount[randomNum] = 1;
@@ -48,7 +52,47 @@ public class GuessingGameTest {
         for (int counter =0; counter<11; counter++) {
             sum+=rndNumCount[counter];
         }
-        System.out.println(sum);
         assertEquals(10, sum);
     }
+
+    @Test
+    public void testFourWrongGuesses(){
+        makeThreeWrongGuesses();
+        String message = game.guess(-3);
+        assertEquals("Game over and you've had 4 tries.", message);
+    }
+
+    private void makeThreeWrongGuesses() {
+        game.guess(-3);
+        game.guess(-3);
+        game.guess(-3);
+    }
+
+    @Test
+    public void testThreeWrongGuessesAndOneCorrect(){
+        makeThreeWrongGuesses();
+        String message = game.guess(game.getRandomNumber());
+        assertEquals("You got it in 4 tries", message);
+
+    }
+
+    @Test
+    public void testTwoWrongGuessesAndOneCorrect(){
+        game.guess(-3);
+        game.guess(-3);
+        String message = game.guess(game.getRandomNumber());
+        assertTrue(message.contains(" 3 "));
+        assertTrue(message.contains("You got it"));
+        assertTrue(message.contains("tries"));
+    }
+
+    @Test
+    public void testTenWrongGuesses(){
+        makeThreeWrongGuesses();
+        makeThreeWrongGuesses();
+        makeThreeWrongGuesses();
+        String message = game.guess(-3);
+        assertEquals("Sorry, you are limited to only 4 tries.", message);
+    }
+
 }
